bc503cff68c9c9fda93d5e104ac38e207006ab14
marcegdv <mgdv79@gmail.com>
2023-05-12 21:13:00 -0300

    added strict mode variable

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
 
 import './styles/vars.css';
 import './index.css';
 
+const USE_STRICT_MODE: boolean = true;
+
+const RenderApp = (): JSX.Element =>
+    USE_STRICT_MODE
+        ? <React.StrictMode>
+            <App />
+        </React.StrictMode>
+        : <App />
+
 const root: ReactDOM.Root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+
+root.render(<RenderApp />);

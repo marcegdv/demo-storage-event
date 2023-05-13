7d0989df9777dcf9c5d6e13ddb5c30424949ea59
marcegdv <mgdv79@gmail.com>
2023-05-12 21:13:55 -0300

    updated and refactored

--- src/components/Containers/Flex/Flex.tsx
+++ src/components/Containers/Flex/Flex.tsx
@@ -1,26 +1,53 @@
+import {
+    OrientationType,
+    InnerSpaceType,
+    PrimaryAxisType,
+    SecondaryAxisType,
+    ColorScaleLevelType,
+} from '../../../types/stylesProps';
+
 import './Flex.css';
+
 
 export type FlexContainerProps = {
     children?: React.ReactNode,
-    orientation?: 'row' | 'column',
-    innerSpace?: '8' | '16',
-    primary?: 'start' | 'center' | 'end' | 'space-between' | 'space-around' | 'space-evenly',
-    secondary?: 'start' | 'center' | 'end' | 'stretch' | 'baseline',
+    orientation?: OrientationType,
+    innerSpace?: InnerSpaceType,
+    primary?: PrimaryAxisType,
+    secondary?: SecondaryAxisType,
+    bgcolorLvl?: ColorScaleLevelType,
+    label?: string,
+    colorLvl?: ColorScaleLevelType,
 };
 
-const FlexContainer = (props: FlexContainerProps): JSX.Element => {
-    const { children, orientation, innerSpace, primary, secondary } = props;
-
-    const classes: string = 'container-flex-' + (orientation ? orientation : 'column') +
+const FlexContainer = (
+    {
+        children,
+        orientation, innerSpace, primary, secondary,
+        bgcolorLvl, label, colorLvl
+    }: FlexContainerProps
+): JSX.Element => {
+    const classes: string =
+        'container-flex-' + (orientation ? orientation : 'column') +
         ' container-flex-inner-space-' + (innerSpace ? innerSpace : '8') +
         ' container-flex-primary-' + (primary ? primary : 'center') +
-        ' container-flex-secondary-' + (secondary ? secondary : 'center')
+        ' container-flex-secondary-' + (secondary ? secondary : 'center') +
+        ' bgcolor-scale-' + (bgcolorLvl ? bgcolorLvl : '0') +
+        ' color-scale-' + (colorLvl ? colorLvl : '0')
         ;
 
     return (
-        <div className={classes}>
-            {children}
-        </div>
+        <>
+            {label
+                ? <fieldset className={classes}>
+                    <legend>{label}</legend>
+                    {children}
+                </fieldset>
+                : <div className={classes}>
+                    {children}
+                </div>
+            }
+        </>
     );
 };
 

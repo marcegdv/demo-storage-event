8aea5fb99fb290e539ab50eb5bf37f12c48ba391
marcegdv <mgdv79@gmail.com>
2023-05-13 04:48:47 -0300

    refactor in type name

--- src/types/stylesProps.ts
+++ src/types/stylesProps.ts
@@ -4,4 +4,4 @@ export type PrimaryAxisType = 'start' | 'center' | 'end' | 'space-between' | 'sp
 export type SecondaryAxisType = 'start' | 'center' | 'end' | 'stretch' | 'baseline';
 export type ColorScaleLevelType = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 'a' | 'b' | 'c' | 'd' | 'e' | 'f';
 
-export type ColorVariant = 'primary' | 'secondary' | 'info' | 'safe' | 'warning' | 'danger';
+export type ColorVariantType = 'primary' | 'secondary' | 'info' | 'safe' | 'warning' | 'danger';

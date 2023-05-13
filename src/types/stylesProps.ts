0a1e5aed17e6ce481c4c9641cc54b8ebdf7cff2f
marcegdv <mgdv79@gmail.com>
2023-05-12 21:14:46 -0300

    types for components  props

---
+++ src/types/stylesProps.ts
@@ -0,0 +1,7 @@
+export type OrientationType = 'row' | 'column';
+export type InnerSpaceType = '8' | '16';
+export type PrimaryAxisType = 'start' | 'center' | 'end' | 'space-between' | 'space-around' | 'space-evenly';
+export type SecondaryAxisType = 'start' | 'center' | 'end' | 'stretch' | 'baseline';
+export type ColorScaleLevelType = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 'a' | 'b' | 'c' | 'd' | 'e' | 'f';
+
+export type ColorVariant = 'primary' | 'secondary' | 'info' | 'safe' | 'warning' | 'danger';
